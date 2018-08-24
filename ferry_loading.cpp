ca58cd378f0fe63d7f3164e783157a352977f759
Martín Mongi Badía <martinmongi@gmail.com>
2018-08-24 13:37:55 +0200

    La base está

--- ferry_loading.cpp
+++ ferry_loading.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <set>
+#include <tuple>
 
 Martin Mongi Badia;
 
@@ -15,53 +17,19 @@ enum Side
     port
 };
 
-typedef struct P
-{
-    int starboard_length;
-    int port_length;
-    vector<int> car_lengths;
-} P;
-
-map<P, vector<Side>> mem;
-
-vector<Side> solve(P &problem)
-{
-    if (problem.starboard_length >= 0 &&
-        problem.port_length >= 0 &&
-        problem.car_lengths.size() == 0)
-    {
-        return vector<Side>();
-    }
-
-    auto it = mem.find(problem);
-    if (it != mem.end())
-    {
-        return it->second;
-    }
-    P problem2;
-    problem2.starboard_length = problem.port_length;
-    problem2.port_length = problem.starboard_length;
-    problem2.car_lengths = problem.car_lengths;
-    auto it = mem.find(problem2);
-    if (it != mem.end())
-    {
-        return it->second;
-    }
-}
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
     int t;
     cin >> t;
+    cout << "CASOS: " << t << '\n';
 
     for (int i = 0; i < t; ++i)
     {
         int boat_length;
         cin >> boat_length;
-        boat_length *= 1000;
-        vector<int> car_lengths;
+        boat_length *= 100;
+        set<tuple<int,int>> sols;
+        sols.insert(tuple<int,int>(0,0));
 
         while (true)
         {
@@ -69,22 +37,24 @@ int main()
             cin >> aux;
             if (aux == 0)
                 break;
-            car_lengths.push_back(aux);
+            set<tuple<int, int>> new_sols;
+            for(auto sol : sols) {
+                if (get<0>(sol) + aux <= boat_length)
+                    new_sols.insert(tuple<int, int>(get<0>(sol) + aux, get<1>(sol)));
+                if (get<1>(sol) + aux <= boat_length)
+                    new_sols.insert(tuple<int, int>(get<0>(sol), get<1>(sol) + aux));
+            }
+            cout << "--------\n";
+            sols = new_sols;
+            for(auto sol : sols) {
+                cout << get<0>(sol) << '\t' << get<1>(sol) << '\n';
+            }
         }
-        sort(car_lengths.begin(), car_lengths.end());
 
-        P problem;
-        problem.starboard_length = boat_length;
-        problem.port_length = boat_length;
-        problem.car_lengths = car_lengths;
-        solve(problem);
     }
     return 0;
 }
 
 
-// Nueva idea:
-// Cada auto que agarro, genero las 2 posibilidades (mandarlo a estribor y a babor)
-// Me quedo con todas las posibilidades que se pueden ir haciendo (puedo tirar casi la mitad,
-// solo guardando las de estribor >= babor), de esta forma no hago los mismos calculos
-// a lo pavote varias veces
+// Hay que toquetear un poco mas al respecto, guardar soluciones, dejar de duplicar casos y demas
+// La base esta
